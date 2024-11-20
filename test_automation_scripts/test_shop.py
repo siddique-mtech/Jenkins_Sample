e92001225bf6ac88a05fe2e3987a0f3f97c88eba
siddique-mtech <112572705+siddique-mtech@users.noreply.github.com>
2024-11-20 12:18:34 +0530

    Update test_shop.py

--- test_automation_scripts/test_shop.py
+++ test_automation_scripts/test_shop.py
@@ -2,16 +2,16 @@ import pytest
 
 @pytest.mark.fruits
 def test_apple():
-   logging.info("This is Apple function")
+   print("This is Apple function")
 
 @pytest.mark.fruits
 def test_banana():
-   logging.info("This is Banana function")
+   print("This is Banana function")
 
 @pytest.mark.veg
 def test_carrot():
-    logging.info("This is Carrot function")
+   print("This is Carrot function")
   
 @pytest.mark.veg
 def test_beetroote():
-    logging.info("This is Beetroote function")
+    print("This is Beetroote function")
